32a7ffc81f3dac51217da10cfa5fd79896ecfea1
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2018-01-14 22:29:31 +0100

    Initial Java EE8 service.

---
+++ src/main/java/de/qaware/qacampus/lombok/JAXRSConfiguration.java
@@ -0,0 +1,11 @@
+package de.qaware.qacampus.lombok;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures a JAX-RS endpoint.
+ */
+@ApplicationPath("api")
+public class JAXRSConfiguration extends Application {
+}
