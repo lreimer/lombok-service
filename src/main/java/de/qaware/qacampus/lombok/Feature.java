11bbeebe4c058e8406b1d6b27aa772dd24863745
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2018-01-17 13:11:23 +0100

    Use single imports.

--- src/main/java/de/qaware/qacampus/lombok/Feature.java
+++ src/main/java/de/qaware/qacampus/lombok/Feature.java
@@ -1,6 +1,10 @@
 package de.qaware.qacampus.lombok;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.json.bind.annotation.JsonbPropertyOrder;
 
